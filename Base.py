85748ce0cec02c92ebea4e088f61de9af7cad90b
Placido <placidodiazgarrido@gmail.com>
2023-10-19 09:08:26 +0200

    Add Base

---
+++ Base.py
@@ -0,0 +1,32 @@
+# Definir variables
+dato1, dato2, dato3, dato4 = 1, 2, 3, 4
+
+# Leer teclado
+print("Introduce un numero")
+num1 = input()
+
+# If tabulado
+if 0 == 0:
+    print("")
+else:
+    print("")
+
+# Bucle for 10 primeros
+for i in range(11):
+    print(i)
+
+# Bucle for | Inicio fin y salto
+for num in range(0,11, 2):
+    print(num)
+
+# Array
+animales = ["perro", "gato"]
+for animal in animales:
+    #la f infica que tiene que tener en cuenta el contenido de {}
+    print(f"{animal}, tamaño {len(animal)}")
+    
+# While
+while dato2 != 10:
+    dato2 += 1
+
+# Comentar muchas líneas con """"
